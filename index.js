884f12bb47739aee884127044ce421e981a3f5c6
rabiaasif <rabiaasif@Rabias-MacBook-Pro-2.local>
2018-07-02 06:51:50 -0400

    getting rid of extra

--- index.js
+++ index.js
@@ -36,7 +36,6 @@ app.post('/webhook/', function(req, res) {
 		let sender = event.sender.id
 		if (event.message && event.message.text) {
 			let text = event.message.text
-			if (text.inclues("hello")) {sendText(sender, "Text echo: " + "not today")};
 			sendText(sender, "Text echo: " + text.substring(0, 100))
 		}
 	}

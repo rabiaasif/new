34b42107ab914af17e5457ebab95229188deb2b6
rabiaasif <rabiaasif@Rabias-MacBook-Pro-2.local>
2018-07-02 07:34:12 -0400

    hi

--- index.js
+++ index.js
@@ -36,7 +36,8 @@ app.post('/webhook/', function(req, res) {
 		let sender = event.sender.id
 		if (event.message && event.message.text) {
 			let text = event.message.text
-			sendText(sender, "Text echo: " + text.substring(0, 100))
+			if (text.includes("hi")) {
+			sendText(sender, "Text echo: " + text.substring(0, 100))};
 		}
 	}
 	res.sendStatus(200)

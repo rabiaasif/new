9426c27249b5f77d10069ab37346f9b6034b7372
rabiaasif <rabiaasif@Rabias-MacBook-Pro-2.local>
2018-07-02 06:42:24 -0400

    try again

--- index.js
+++ index.js
@@ -36,12 +36,13 @@ app.post('/webhook/', function(req, res) {
 		let sender = event.sender.id
 		if (event.message && event.message.text) {
 			let text = event.message.text
-			
+			if (text.inclues("hello")) {sendText(sender, "Text echo: " + "not today"};
 			sendText(sender, "Text echo: " + text.substring(0, 100))
 		}
 	}
 	res.sendStatus(200)
 })
+
 
 function sendText(sender, text) {
 	let messageData = {text: text}
